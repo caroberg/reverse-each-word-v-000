8a24dc585c790ffbba52617d672fd8cfa18dfd2c
Caroline Berg <carolineeberg@gmail.com>
2018-08-06 15:33:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word
-  new_array = "Hello there, and how are you?".each_char {|c| print c.split}
-  new_array.reverse
+def reverse_each_word(words)
+  words = "Hello there, and how are you?".each_char {|c| print c.split}
+  words.reverse
 end
