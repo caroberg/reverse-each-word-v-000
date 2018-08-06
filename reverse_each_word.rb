ee921ac6d8eb0283bbe425ccbd9975c4719f3a03
Caroline Berg <carolineeberg@gmail.com>
2018-08-06 15:43:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
+words = "Hello there, and how are you?"
 def reverse_each_word(words)
-  words = "Hello there, and how are you?".each_char {|c| print c.split}
+  words.each_char {|c| print c.split}
   words.reverse
 end
