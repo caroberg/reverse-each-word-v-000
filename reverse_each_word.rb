1fd6912b8d56700bc0e2b01152557fa858ef5fea
Caroline Berg <carolineeberg@gmail.com>
2018-08-06 16:18:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(words)
 words_array = words.split
   words_array.each do |word|
-  words_string = "#{word.reverse!} "
-  words_string.join
+  print "#{word.reverse!} "
   end
 end
