e1559c6bbfc8a50e2d58daf7ec3532ab2c14d84a
Caroline Berg <carolineeberg@gmail.com>
2018-08-06 15:53:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 words = "Hello there, and how are you?"
 def reverse_each_word(words)
-new_array = words.split 
+new_array = words.split
   new_array.each do |word|
   print "#{word.reverse} "
   end
